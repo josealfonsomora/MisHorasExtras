b7203506f44e141f76418be7224118228c96903f
Jose Alfonso Mora Lores <josealfonsomora@Mac-Pro.local>
2020-07-31 23:37:22 +0200

    Add date picker

--- app/src/main/java/com/josealfonsomora/mishorasextras/ui/AddHoraExtraFragment.kt
+++ app/src/main/java/com/josealfonsomora/mishorasextras/ui/AddHoraExtraFragment.kt
@@ -1,5 +1,7 @@
 package com.josealfonsomora.mishorasextras.ui
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,9 +16,13 @@ import com.josealfonsomora.mishorasextras.db.HoraExtraDao
 import com.josealfonsomora.mishorasextras.extensions.print
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import org.threeten.bp.LocalDate
+import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 import java.math.BigDecimal
+import java.util.*
 import javax.inject.Inject
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -59,7 +65,24 @@ class AddHoraExtraFragment : Fragment() {
                     )
                 }
                 findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            }
 
+            dateTextView.setOnClickListener {
+                val today = LocalDate.now()
+                // date picker dialog
+                // date picker dialog
+                val picker = DatePickerDialog(
+                    requireContext(),
+                    OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                        dateTextView.setText(
+                            dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year
+                        )
+                        dateTextView.setTag(
+                            LocalDate.of(year, monthOfYear, dayOfMonth).atStartOfDay(ZoneId.systemDefault()).toEpochSecond()
+                        )
+                    }, today.year, today.monthValue, today.dayOfMonth
+                )
+                picker.show()
             }
         }
     }
